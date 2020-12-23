247097c63c1c3ab30bdb23b9f0d04aafcf84797e
Isaac <isaacv49@gmail.com>
2020-12-23 05:31:27 -0600

    adding multiply and divide functions to calculator

--- test_calculator.py
+++ test_calculator.py
@@ -10,14 +10,14 @@ class TestCalculator:
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
-    
+
     def test_subtraction(self):
-        assert 5 == calculator.subtract(10,5)
+        assert 5 == calculator.subtract(10, 5)
 
 
     def test_multiply(self):
-        assert 100 == calculator.multiply(10,10)
+        assert 100 == calculator.multiply(10, 10)
 
 
     def test_divide(self):
-        assert 10 == calculator.divide(100,10)
+        assert 10 == calculator.divide(100, 10)
