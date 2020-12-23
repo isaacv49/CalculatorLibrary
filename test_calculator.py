53a2a6d3d16bd4fa7930e8504179324295f16676
Isaac <isaacv49@gmail.com>
2020-12-23 05:27:08 -0600

    adding multiply and divide tests

--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,15 @@ class TestCalculator:
 
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
+
+    
+    def test_subtraction(self):
+        assert 5 == calculator.subtract(10,5)
+
+
+    def test_multiply(self):
+        assert 100 == calculator.multiply(10,10)
+
+
+    def test_divide(self):
+        assert 10 == calculator.divide(100,10)
